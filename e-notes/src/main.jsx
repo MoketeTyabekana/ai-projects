0626fa509e87f2a6d7ba61dace194a3ef3595d67
Mokete <ematyabekana@gmail.com>
2025-04-06 15:52:51 +0200

    Add speech recognition polyfill to main entry point

--- e-notes/src/main.jsx
+++ e-notes/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Add speech recognition polyfill
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
